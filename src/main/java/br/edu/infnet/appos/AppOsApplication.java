c1e205ee8b93c0497f41a50ba0dd13e9e4d26264
Augusto Cezar Ribeiro Santana <augusto.santana@al.infnet.edu.br>
2022-08-03 20:12:45 -0300

    Initial commit

---
+++ src/main/java/br/edu/infnet/appos/AppOsApplication.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.appos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppOsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppOsApplication.class, args);
+	}
+
+}
