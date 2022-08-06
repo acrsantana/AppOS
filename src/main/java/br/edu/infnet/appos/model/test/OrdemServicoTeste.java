94ef866c30fb65a7b2a8a43c5eb5a7d3bae5ed94
Augusto Cezar Ribeiro Santana <cezaodabahia@gmail.com>
2022-08-05 21:10:35 -0300

    Criação dos atributos da classes de domínio e dos seu respectivos testes: itens 1 e 2

--- src/main/java/br/edu/infnet/appos/model/test/OrdemServicoTeste.java
+++ src/main/java/br/edu/infnet/appos/model/test/OrdemServicoTeste.java
@@ -1,4 +1,27 @@
 package br.edu.infnet.appos.model.test;
 
+import java.time.LocalDateTime;
+
 public class OrdemServicoTeste {
+
+    public static void main(String[] args) {
+        String descricao = "Reparo no ar condicionado";
+        LocalDateTime data = LocalDateTime.now();
+        boolean garantia = true;
+
+        String descricao2 = "Troca do escapamento";
+        LocalDateTime data2 = LocalDateTime.now();
+        boolean garantia2 = false;
+
+        String descricao3 = "Limpeza da cabine";
+        LocalDateTime data3 = LocalDateTime.now();
+        boolean garantia3 = true;
+
+        System.out.println(descricao + ";" + data + ";" + garantia);
+        System.out.println(descricao2 + ";" + data2 + ";" + garantia2);
+        System.out.println(descricao3 + ";" + data3 + ";" + garantia3);
+    }
+
+
+
 }
